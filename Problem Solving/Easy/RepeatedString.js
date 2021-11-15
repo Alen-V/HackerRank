1a7ce4e30372fd00bdb070aabda00b145b1547fd
Alen Vatic <54191137+Alen-V@users.noreply.github.com>
2021-11-15 09:22:19 +0100

    update

--- Problem Solving/Easy/RepeatedString.js
+++ Problem Solving/Easy/RepeatedString.js
@@ -1,13 +1,13 @@
+const countOccurence = (s, count = 0) => {
+    const stringArr = s.split('')
+    for(let i = 0; i < stringArr.length; i++)
+        if(stringArr[i] === 'a')
+            count++
+    return count
+}
 function repeatedString(s, n) {
     if(s === 'a') {
         return n
-    }
-    function countOccurence(s, count = 0) {
-        const stringArr = s.split('')
-        for(let i = 0; i < stringArr.length; i++)
-            if(stringArr[i] === 'a')
-                count++
-        return count
     }
     const stringLength = s.length
     const remainder = n % stringLength
